0fb42f286b9da98d0ea88f0ec0a900854ac27872
Pan Aditya <aditya.pan67@titansoft.com.sg>
2020-06-28 16:04:49 +0800

    first commit, full working demo

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Testing Greeter
+            // var input = new HelloRequest{ Name = "Alex"};
+            // var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            // var client = new Greeter.GreeterClient(channel);
+            //
+            // var reply = await client.SayHelloAsync(input);
+            //
+            // Console.WriteLine(reply.Message);
+
+            //Testing Games
+            var input1 = new OsRequest{UserId = 235};
+            var input2 = new OsRequest{UserId = 234};
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Games.GamesClient(channel);
+
+            var reply1 = await client.GetUserOsAsync(input1);
+            var reply2 = await client.GetUserOsAsync(input2);
+
+            Console.WriteLine(reply1.Useros);
+            Console.WriteLine(reply2.Useros);
+
+            using (var newGamesCall = client.GetNewGames(new NewGamesRequest()))
+            {
+                while (await newGamesCall.ResponseStream.MoveNext())
+                {
+                    var currentGame = newGamesCall.ResponseStream.Current;
+                        
+                    Console.WriteLine($"{currentGame.Name} {currentGame.Price} \nGraphics Card Required: {currentGame.Requirement.NeedsGraphicsCard}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/GamesService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServer.Services
+{
+    public class GamesService : Games.GamesBase
+    {
+        private readonly ILogger<GamesService> _logger;
+        public GamesService(ILogger<GamesService> logger)
+        { 
+            _logger = logger;
+        }
+
+        public override Task<OsResponse> GetUserOs(OsRequest request, ServerCallContext context)
+        {
+            OsResponse response = new OsResponse();
+            if (request.UserId == 234)
+            {
+                response.Useros = "windows";
+            }
+            else
+            {
+                response.Useros = "linux";
+            }
+
+            return Task.FromResult(response);
+        }
+
+        public override async Task GetNewGames(NewGamesRequest request, IServerStreamWriter<NewGamesResponse> responseStream, ServerCallContext context)
+        {
+            List<NewGamesResponse> response = new List<NewGamesResponse>
+            {
+                new NewGamesResponse
+                {
+                    Name = "Duty Calls",
+                    Price = 24.33f,
+                    Requirement = new NewGamesRequirement
+                    {
+                        IsLinuxSupported = true,
+                        IsWindowsSupported = true,
+                        NeedsGraphicsCard = false
+                    }
+                },
+                new NewGamesResponse
+                {
+                    Name = "Ultimate 8k Fighter",
+                    Price = 124.33f,
+                    Requirement = new NewGamesRequirement
+                    {
+                        IsLinuxSupported = false,
+                        IsWindowsSupported = true,
+                        NeedsGraphicsCard = true
+                    }
+                },
+                new NewGamesResponse
+                {
+                    Name = "Alpha Test Go",
+                    Price = 14.33f,
+                    Requirement = new NewGamesRequirement
+                    {
+                        IsLinuxSupported = true,
+                        IsWindowsSupported = false,
+                        NeedsGraphicsCard = false
+                    }
+                }
+            };
+
+            foreach (var gamesResponse in response)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(gamesResponse);
+            }
+        }
+    }
+}
